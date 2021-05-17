afbb38712f2e54fe87ed0efc18f84a16f5db6225
Meshach Adoe <madoe@ucsd.edu>
2021-05-16 19:54:31 -0700

    Configured Parse backend and implemented login/signup functionality

---
+++ OnTrack/OnTrack/SignUpViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SignUpViewController.swift
+//  OnTrack
+//
+//  Created by Meshach Adoe on 15/05/21.
+//
+
+import UIKit
+import Parse
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { success, error in
+            if success {
+                print("Success in registering: \(success)")
+                //self.performSegue(withIdentifier: "signUpSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
