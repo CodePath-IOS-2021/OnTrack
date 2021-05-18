89fc6288dfcd899defedd40ec8d87b6ec27440b9
DameDollaForThree <j1cai@ucsd.edu>
2021-05-17 18:32:36 -0700

    first commit on AddMealPlan screen

---
+++ OnTrack/OnTrack/CaloryTagCell.swift
@@ -0,0 +1,13 @@
+//
+//  CaloryTagCell.swift
+//  OnTrack
+//
+//  Created by  caijicang on 2021/5/14.
+//
+
+import UIKit
+import MaterialComponents.MaterialChips
+
+class CaloryTagCell: MDCChipCollectionViewCell {
+    
+}
---
+++ OnTrack/OnTrack/MealTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  BreakfastTVCell.swift
+//  OnTrack
+//
+//  Created by  caijicang on 2021/5/15.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var recipeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
