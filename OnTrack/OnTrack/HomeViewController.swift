0abf55d2222a90cc9daf21b8b69bb52945e36825
Meshach Adoe <madoe@ucsd.edu>
2021-05-29 12:58:52 -0700

    Implement community section and refine auto layout for today collection view

--- OnTrack/OnTrack/HomeViewController.swift
+++ OnTrack/OnTrack/HomeViewController.swift
@@ -10,7 +10,7 @@ import Parse
 
 class HomeViewController: UIViewController,UITableViewDelegate, UITableViewDataSource, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    @IBOutlet weak var todayCollectionViewCell: UICollectionViewCell!
+    @IBOutlet weak var todayCollectionView: UICollectionView!
     @IBOutlet weak var communityTableView: UITableView!
     
     var allMealPlan = [PFObject]()
@@ -22,10 +22,9 @@ class HomeViewController: UIViewController,UITableViewDelegate, UITableViewDataS
         communityTableView.dataSource = self
         communityTableView.delegate = self
         
-       /* todayCollectionViewCell.delegate = self
-        todayCollectionViewCell.dataSource = self*/
-
-        // Do any additional setup after loading the view.
+        
+        todayCollectionView.delegate = self
+        todayCollectionView.dataSource = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -39,6 +38,10 @@ class HomeViewController: UIViewController,UITableViewDelegate, UITableViewDataS
                 self.communityTableView.reloadData()
             }
         }
+        
+        let user = PFUser.current()!
+        myMealPlan = user["meal_plans"] as! [PFObject]
+        todayCollectionView.reloadData()
     }
     
     // NOTE: IMAGE SHOULD BE STORED IN BACK4APP, SO WE CAN ACCESS IT THROUGH THE CURRENT USER, IMAGE SHOULD BE APART OF IT, AFTER THEY ADDED IT IN RECIPEVIEWCONTROLLER
@@ -60,21 +63,33 @@ class HomeViewController: UIViewController,UITableViewDelegate, UITableViewDataS
         cell.mealLabel.text = breakfastMeals[0]["label"] as! String
         cell.calorieLabel.text = breakfastMeals[0]["calories"] as! String
         
-        let mealURL = URL(string :"https://www.closetcooking.com/wp-content/uploads/2018/03/TonkotsuRamen8000837-min.jpg");
+        let mealURL = URL(string :breakfastMeals[0]["image"] as! String);
         cell.mealImageView.af_setImage(withURL: mealURL!)
         
-        return cell 
+        return cell
 
     }
     
     // COLLECTION VIEW: For today's planned meals cards
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if myMealPlan.count > 0 {
+            return 3
+        }
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "todayCollectionViewCell", for: indexPath) as! todayCollectionViewCell
-        
+        if myMealPlan.count > 0 {
+            let mealPlan = myMealPlan[0]
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "todayCollectionViewCell", for: indexPath) as! todayCollectionViewCell
+            
+            //let breakfastMeals = (mealPlan["breakfast_recipes"] as? [PFObject]) ?? []
+            //print(breakfastMeals)
+            //cell.dish1NameLabel.text = breakfastMeals[0]["label"] as! String
+            
+            return cell
+        }
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "defaultTodayCollectionViewCell", for: indexPath) as! defaultTodayCollectionViewCell
         return cell
     }
     
--- OnTrack/OnTrack/todayCollectionViewCell.swift
+++ OnTrack/OnTrack/todayCollectionViewCell.swift
@@ -9,5 +9,25 @@ import UIKit
 
 class todayCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var dish1NameLabel: UILabel!
+    @IBOutlet weak var dish1CalorieLabel: UILabel!
     
+    @IBOutlet weak var dish2NameLabel: UILabel!
+    @IBOutlet weak var dish2CalorieLabel: UILabel!
+    
+    @IBOutlet weak var dish3NameLabel: UILabel!
+    @IBOutlet weak var dish3CalorieLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Apply rounded corners
+        contentView.layer.cornerRadius = 5.0
+        contentView.layer.masksToBounds = true
+                
+        // Set masks to bounds to false to avoid the shadow
+        // from being clipped to the corner radius
+        layer.cornerRadius = 5.0
+        layer.masksToBounds = false
+    }
+
 }
